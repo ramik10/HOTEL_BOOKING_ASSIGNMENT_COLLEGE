3413561953b544d99293689a07095e5757e41b96
Ramik Mukherjee <ramik.14062000@gmail.com>
2024-05-17 16:16:06 +0530

    update 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 const corsOptions: cors.CorsOptions = {
     origin: "*",
